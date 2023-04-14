31fef1a00e6b95d1da23ee01dc8621c679537cf0
GabrielReds13 <92747698+GabrielReds13@users.noreply.github.com>
2023-04-14 14:32:42 -0400

    Fixed "EMPATE" bug

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 
 public class Program
 {
@@ -12,60 +12,77 @@ public class Program
         string[] ocupacao = n.GetOcupacao();
 
         // Codigo
-        while(partida == false) {
-          Console.Clear();
-
-          // Movimentacao
-          Console.WriteLine(" -- MOVIMENTAÇÃO --");
-          Console.WriteLine(" - CIMA [ w ]\n - BAIXO [ s ]\n - DIREITA [ d ]\n - ESQUERDA [ a ]\n");
-
-          // Outras configuracoes
-          Console.WriteLine(" -- OUTROS --");
-          Console.WriteLine(" - ESCOLHER/CONFIRMAR [ espaço ]\n");
-
-          // Iniciar partida
-          Console.WriteLine(" ! PRESSIONE [ espaço ] PARA INICIAR PARTIDA !");
-          iniciarJogo = Console.ReadKey();
-
-          if($"{iniciarJogo.Key}" == "Spacebar") {
-            partida = true;
-          } else {
+        while (partida == false)
+        {
             Console.Clear();
-            Console.WriteLine(" ! ERROR: TECLA INVÁLIDA PARA INICIAR JOGO !");
-            Console.ReadKey();
-          }
+
+            // Movimentacao
+            Console.WriteLine(" -- MOVIMENTAÇÃO --");
+            Console.WriteLine(" - CIMA [ w ]\n - BAIXO [ s ]\n - DIREITA [ d ]\n - ESQUERDA [ a ]\n");
+
+            // Outras configuracoes
+            Console.WriteLine(" -- OUTROS --");
+            Console.WriteLine(" - ESCOLHER/CONFIRMAR [ espaço ]\n");
+
+            // Iniciar partida
+            Console.WriteLine(" ! PRESSIONE [ espaço ] PARA INICIAR PARTIDA !");
+            iniciarJogo = Console.ReadKey();
+
+            if ($"{iniciarJogo.Key}" == "Spacebar")
+            {
+                partida = true;
+            }
+            else
+            {
+                Console.Clear();
+                Console.WriteLine(" ! ERROR: TECLA INVÁLIDA PARA INICIAR JOGO !");
+                Console.ReadKey();
+            }
         }
 
         // Jogo
-        while(partida == true) {
-          Console.Clear();
+        while (partida == true)
+        {
+            Console.Clear();
 
-          // Grade e turno
-          Console.WriteLine($"* VEZ DE: {n.TrocarTurno()}\n");
-          n.Grade();
+            // Grade e turno
+            Console.WriteLine($"* VEZ DE: {n.TrocarTurno()}\n");
+            n.Grade();
 
-          // Mostrar cordenadas
-          n.Cordenadas();
-          
-          // Verificar vitoria
-          n.VerificarVitoria();
-          if(n.VerificarVitoria() == ocupacao[1] || n.VerificarVitoria() == ocupacao[2]) {
-            partida = false;
-            Console.WriteLine($"VENCEDOR: {n.VerificarVitoria()}");
-            Console.ReadKey();
+            // Mostrar cordenadas
+            n.Cordenadas();
+
+            // Verificar vitoria
+            n.VerificarVitoria();
+            if (n.VerificarVitoria() == ocupacao[1] || n.VerificarVitoria() == ocupacao[2])
+            {
+                // Imprimir vencedor
+                partida = false;
+                Console.WriteLine($"VENCEDOR: {n.VerificarVitoria()}");
+                Console.ReadKey();
+            } 
+            // Empate
+            else if(n.VerificarVitoria() == "EMPATE")
+            {
+                partida = false;
+                Console.WriteLine($"VENCEDOR: {n.VerificarVitoria()}");
+                Console.ReadKey();
+            }
 
             // Finalizar partida
-            Console.Clear();
-            Console.WriteLine("\n- PRESSIONE QUALQUER TECLA PARA SAIR -");
-            Console.WriteLine("        - OBRIGADO POR JOGAR - ");
-            Console.ReadKey();
-            break;
-          }
+            if(partida != true)
+            {
+                Console.Clear();
+                Console.WriteLine("\n- PRESSIONE QUALQUER TECLA PARA SAIR -");
+                Console.WriteLine("        - OBRIGADO POR JOGAR - ");
+                Console.ReadKey();
+                break;
+            }
 
-          // Movimentacao
-          tecla = Console.ReadKey();
-          n.Controle(tecla);
-          n.EscolherCasa(tecla);
+            // Movimentacao
+            tecla = Console.ReadKey();
+            n.Controle(tecla);
+            n.EscolherCasa(tecla);
         }
     }
 }
